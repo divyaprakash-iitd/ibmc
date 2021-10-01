5229721a7d2b513e7b1d80d4dcfb5c52ff6a320a
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-01 15:44:11 +0530

    Added lines to generate staggered mesh for the given mesh size

--- analyze_data.m
+++ analyze_data.m
@@ -8,25 +8,39 @@ yG = Ghia.U(:,2);
 %% Reynolds Number (100, 400 & 1000)
 % To run a case uncomment one of the following three lines
 
-nx = 50; ny = 50; Re = 100; UG = Ghia.U(:,3); 
+nx = 20; ny = 20; Re = 100; UG = Ghia.U(:,3); 
 %nx = 150; ny = 150; Re = 400; UG = Ghia.U(:,4);
 %nx = 200; ny = 200; Re = 1000; UG = Ghia.U(:,5);
 
 %% Compare the velocity
 
-% Load mesh data
-x = load('x.txt');
-y = load('y.txt');
-xm = load('xm.txt');
-ym = load('ym.txt');
+% % Load mesh data
+% x = load('x.txt');
+% y = load('y.txt');
+% xm = load('xm.txt');
+% ym = load('ym.txt');
+
+imin = 2;
+imax = imin+nx-1;
+jmin = 2;
+jmax = jmin+ny-1;
+
+Lx = 1;
+Ly = 1;
+
+% Create Mesh
+x(imin:imax+1) = linspace(0,Lx,nx+1);
+y(jmin:jmax+1) = linspace(0,Ly,ny+1);
+xm(imin:imax)  = 0.5*(x(imin:imax)+x(imin+1:imax+1));
+ym(jmin:jmax)  = 0.5*(y(jmin:jmax)+y(jmin+1:jmax+1));
 
 dx = x(3)-x(2);
 dy = y(3)-y(2);
 
-x = x(2:end);
-y = y(2:end);
-xm = xm(2:end);
-ym = ym(2:end);
+x = x(2:end)';
+y = y(2:end)';
+xm = xm(2:end)';
+ym = ym(2:end)';
 
 ux = x;
 uy = [ym(1)-dy;ym;ym(end)+dy];
