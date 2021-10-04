4c7e39dfba225a57ff2dc7e8901966721a102111
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-04 11:20:17 +0530

    Added a module to delegate the time loop from the main program

---
+++ mod_time.f90
@@ -0,0 +1,10 @@
+module mod_time
+    use iso_fortran_env, only: int32, real32, real64
+    implicit none
+    
+contains
+
+    subroutine time_loop()
+    end subroutine time_loop 
+
+end module mod_time
