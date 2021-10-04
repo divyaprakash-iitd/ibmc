d7f734a8bfe9a1819971314d4cdfc8652949e87f
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-04 14:57:32 +0530

    Added a subroutine for the corrector step

--- mod_time.f90
+++ mod_time.f90
@@ -5,7 +5,7 @@ module mod_time
     
 contains
 
-    subroutine predictor(M,u,v,us,vs,nu,dt)
+    pure subroutine predictor(M,u,v,us,vs,nu,dt)
         class(mesh), intent(in) :: M
         real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
@@ -30,7 +30,6 @@ contains
                         - vcenter*(u(i,j+1)-u(i,j-1))*0.5*dyi)
         end do
 
-        print *, norm2(us)
         ! vs 
         ! (v-velocity cell)
         do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
@@ -42,6 +41,33 @@ contains
                         - v(i,j)*(v(i,j+1)-v(i,j-1))*0.5*dyi)
         end do
 
-    end subroutine predictor 
+    end subroutine predictor
+    
+    pure subroutine corrector(M,u,v,us,vs,p,rho,dt)
+        class(mesh), intent(in) :: M
+        real(real64), intent(in out) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: P(M%xp%lb:M%xp%ub,M%yp%lb:M%yp%ub)
+        real(real64), intent(in) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real32), intent(in) :: rho, dt
+
+        integer(int32) :: i,j
+        real(real64) :: dxi, dyi
+
+        dxi = 1/M%dx
+        dyi = 1/M%dy
+
+        ! Perform the corrector steps
+        ! u 
+        ! (u-velocity cell)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
+            u(i,j) = us(i,j) - dt/rho * (p(i,j) - p(i-1,j)) * dxi
+        end do
+        ! 
+        ! v
+        ! (v-velocity cell)
+        do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
+            v(i,j) = vs(i,j) - dt/rho * (p(i,j) - p(i,j-1)) * dyi
+        end do
+    end subroutine corrector
 
 end module mod_time
