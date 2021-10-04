6c6a3f728b5a6a56590390522b9cdb4ff98b29c8
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-04 12:36:56 +0530

    Successfully changed the indices range to mesh reference

--- ibmc.f90
+++ ibmc.f90
@@ -19,7 +19,7 @@ program ibmc
     real(real32) :: dy, dyi
 
     ! Simulation Paramaters
-    real(real32) :: tsim    = 2
+    real(real32) :: tsim    = 1
     real(real32) :: dt      = 0.001
     real(real32) :: t
 
@@ -51,6 +51,9 @@ program ibmc
     type(mesh) :: M
 
     call cpu_time(start)
+
+    ! Construct Mesh
+    M = mesh('M',Lx,Ly,Nx,Ny)
 
     ! Define boundary conditions
     utop    = 1.0
@@ -145,7 +148,7 @@ program ibmc
         ! Perform predictor step
         ! us 
         ! (u-velocity cell)
-        do concurrent (j = jucS:jucE, i = iucS:iucE)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
             vcenter = 0.25*(v(i-1,j) + v(i-1,j+1) + v(i,j) + v(i,j+1))
             us(i,j) = u(i,j) + dt* &
                         ( nu*(u(i-1,j) - 2*u(i,j) + u(i+1,j))*dxi**2 &
@@ -156,7 +159,7 @@ program ibmc
 
         ! vs 
         ! (v-velocity cell)
-        do concurrent (j = jvcS:jvcE, i = ivcS:ivcE)
+        do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
             ucenter = 0.25*(u(i,j-1)+u(i,j)+u(i+1,j-1)+u(i+1,j))
             vs(i,j) = v(i,j) + dt* &
                         ( nu*(v(i-1,j) - 2*v(i,j) + v(i+1,j))*dxi**2 &
@@ -167,7 +170,7 @@ program ibmc
 
         ! Form the right hand side of the pressure poisson equation
         ! (Pressure cell)
-        do concurrent (j = jpS:jpE, i = ipS:ipE)
+        do concurrent (j = M%yp%lb:M%yp%ub, i = M%xp%lb:M%xp%ub)
             R(i,j) = -rho/dt* &
                     ( (us(i+1,j) - us(i,j))*dxi &
                     + (vs(i,j+1) - vs(i,j))*dyi)
@@ -181,13 +184,13 @@ program ibmc
         ! Perform the corrector steps
         ! u 
         ! (u-velocity cell)
-        do concurrent (j = jucS:jucE, i = iucS:iucE)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
             u(i,j) = us(i,j) - dt/rho * (p(i,j) - p(i-1,j)) * dxi
         end do
         
         ! v
         ! (v-velocity cell)
-        do concurrent (j = jvcS:jvcE, i = ivcS:ivcE)
+        do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
             v(i,j) = vs(i,j) - dt/rho * (p(i,j) - p(i,j-1)) * dyi
         end do
 
