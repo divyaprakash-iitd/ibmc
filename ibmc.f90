3ef2cf656e8235c328c648d335aca643b0a59914
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-04 14:48:14 +0530

    Delegated the application of boundary conditions to a subroutine

--- ibmc.f90
+++ ibmc.f90
@@ -4,6 +4,7 @@ program ibmc
     use mod_amgx, only: calculate_pressure_amgx
     use mod_mesh
     use mod_time
+    use mod_boundary
     implicit none
 
     ! Execution time
@@ -92,41 +93,10 @@ program ibmc
         t = t+dt 
 
         ! Apply boundary conditions
-        u(:,M%yu%lb) = 2*ubottom - u(:,M%yu%lb+1);
-        u(:,M%yu%ub) = 2*utop    - u(:,M%yu%ub-1);
-        v(M%xv%lb,:) = 2*vleft   - v(M%xv%lb+1,:);
-        v(M%yv%ub,:) = 2*vright  - v(M%yv%ub-1,:);
-    
-
-        u(M%xu%lb,:)    = uleft;
-        u(M%xu%ub,:)    = uright;
-        v(:,M%yv%ub)    = vtop;
-        v(:,M%yv%lb)    = vbottom;
+        call apply_boundary(M,u,v,utop,ubottom,uleft,uright,vtop,vbottom,vleft,vright)
 
         ! Perform predictor step
-        ! ! us 
-        ! ! (u-velocity cell)
-        ! do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
-        !     vcenter = 0.25*(v(i-1,j) + v(i-1,j+1) + v(i,j) + v(i,j+1))
-        !     us(i,j) = u(i,j) + dt* &
-        !                 ( nu*(u(i-1,j) - 2*u(i,j) + u(i+1,j))*dxi**2 &
-        !                 + nu*(u(i,j-1) -2*u(i,j) + u(i,j+1))*dyi**2 &
-        !                 - u(i,j)*(u(i+1,j) - u(i-1,j))*0.5*dxi &
-        !                 - vcenter*(u(i,j+1)-u(i,j-1))*0.5*dyi)
-        ! end do
-
-        ! ! vs 
-        ! ! (v-velocity cell)
-        ! do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
-        !     ucenter = 0.25*(u(i,j-1)+u(i,j)+u(i+1,j-1)+u(i+1,j))
-        !     vs(i,j) = v(i,j) + dt* &
-        !                 ( nu*(v(i-1,j) - 2*v(i,j) + v(i+1,j))*dxi**2 &
-        !                 + nu*(v(i,j-1) - 2*v(i,j) + v(i,j+1))*dyi**2 &
-        !                 - ucenter*(v(i+1,j) - v(i-1,j))*0.5*dxi &
-        !                 - v(i,j)*(v(i,j+1)-v(i,j-1))*0.5*dyi)
-        ! end do
-
-       call predictor(M,u,v,us,vs,nu,dt) 
+        call predictor(M,u,v,us,vs,nu,dt) 
         
         ! Form the right hand side of the pressure poisson equation
         ! (Pressure cell)
@@ -142,17 +112,7 @@ program ibmc
         !call calculate_pressure_amgx(A,P,R,init_status)
 
         ! Perform the corrector steps
-        ! u 
-        ! (u-velocity cell)
-        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
-            u(i,j) = us(i,j) - dt/rho * (p(i,j) - p(i-1,j)) * dxi
-        end do
-        
-        ! v
-        ! (v-velocity cell)
-        do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
-            v(i,j) = vs(i,j) - dt/rho * (p(i,j) - p(i,j-1)) * dyi
-        end do
+        call corrector(M,u,v,us,vs,p,rho,dt)
 
         print *, 'time = ', t
     end do
