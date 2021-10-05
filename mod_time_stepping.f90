76a68282b89693ac1310f3d12d5c69e3b2617c9d
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-05 13:25:50 +0530

    Reimplemented the RK4 method

--- mod_time_stepping.f90
+++ mod_time_stepping.f90
@@ -5,16 +5,15 @@ module mod_time_stepping
     
 contains
 
-    subroutine euler(M,u,v,us,vs,nu,dt)
+    pure subroutine euler(M,u,v,us,vs,nu,dt)
         class(mesh), intent(in) :: M
         real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real32), intent(in) :: nu, dt
     
-        ! print *, lbound(fu(M,u,v,nu),1)
         
-        call fu(M,u,v,us,nu)
-        call fv(M,u,v,vs,nu) 
+        call cdu(M,u,v,us,nu)
+        call cdv(M,u,v,vs,nu) 
 
         us = u + dt*us
         vs = v + dt*vs
@@ -31,16 +30,16 @@ contains
         real(real64) :: du2(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), dv2(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64) :: tmpu(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), tmpv(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         
-        call fu(M,u,v,tmpu,nu)
+        call cdu(M,u,v,tmpu,nu)
         du1 = real(dt,real64)*tmpu
 
-        call fv(M,u,v,tmpv,nu)
+        call cdv(M,u,v,tmpv,nu)
         dv1 = real(dt,real64)*tmpv
         
-        call fu(M,(u+du1),(v+dv1),tmpu,nu)
+        call cdu(M,(u+du1),(v+dv1),tmpu,nu)
         du2 = real(dt,real64)*tmpu
         
-        call fv(M,(u+du1),(v+dv1),tmpv,nu)
+        call cdv(M,(u+du1),(v+dv1),tmpv,nu)
         dv2 = real(dt,real64)*tmpv
 
 
@@ -54,41 +53,41 @@ contains
         real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real32), intent(in) :: nu, dt
 
-        real(real64) :: h
         real(real64) :: du1(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), dv1(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64) :: du2(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), dv2(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64) :: du3(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), dv3(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
-        real(real64) :: du4(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), dv4(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
-        real(real64) :: tmpu(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), tmpv(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
-       
-        h = real(dt,real64)
+        real(real64) :: k1u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k1v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64) :: k2u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k2v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64) :: k3u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k3v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64) :: k4u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k4v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
 
         ! Step 1
-        call fu(M,u,v,tmpu,nu)
-        du1 = tmpu*h/2
-        call fv(M,u,v,tmpv,nu)
-        dv1 = tmpv*h/2
+        call cdu(M,u,v,k1u,nu)
+        du1 = k1u*dt/2
+        call cdv(M,u,v,k1v,nu)
+        dv1 = k1v*dt/2
         
         ! Step 2
-        call fu(M,(u+du1),(v+dv1),tmpu,nu)
-        du2 = h*tmpu/2
-        call fv(M,(u+du1),(v+dv1),tmpv,nu)
-        dv2 = h*tmpv/2
+        call cdu(M,(u+du1),(v+dv1),k2u,nu)
+        du2 = k2u*dt/2
+        call cdv(M,(u+du1),(v+dv1),k2v,nu)
+        dv2 = k2v*dt/2
 
         ! Step 3
-        call fu(M,(u+du2),(v+dv2),tmpu,nu)
-        du3 = h*tmpu/2
-        call fv(M,(u+du2),(v+dv2),tmpv,nu)
-        dv3 = h*tmpv/2
+        call cdu(M,(u+du2),(v+dv2),k3u,nu)
+        du3 = dt*k3u
+        call cdv(M,(u+du2),(v+dv2),k3v,nu)
+        dv3 = dt*k3v
 
         ! Step 4
-        call fu(M,(u+du3),(v+dv3),tmpu,nu)
-        du4 = tmpu
-        call fv(M,(u+du3),(v+dv3),tmpv,nu)
-        dv4 = tmpv
+        call cdu(M,(u+du3),(v+dv3),k4u,nu)
+        call cdv(M,(u+du3),(v+dv3),k4v,nu)
 
-        us = u + 1.0d0/6*h*(du1*2/h+2*du2+2*du3+du4)
-        vs = v + 1.0d0/6*h*(dv1*2/h+2*dv2+2*dv3+dv4)
+        us = u + 1.0d0/6*dt*(k1u + 2*k2u + 2*k3u + k4u)
+        vs = v + 1.0d0/6*dt*(k1v + 2*k2v + 2*k3v + k4v)
+
+        ! us = us/2
+        ! vs = vs/2
 
     end subroutine RK4
 
