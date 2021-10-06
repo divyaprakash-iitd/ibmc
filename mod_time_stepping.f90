847405c4c4b80f4a33c90cf2590d8bb0ccb38cea
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-06 15:12:55 +0530

    minor changes

--- mod_time_stepping.f90
+++ mod_time_stepping.f90
@@ -60,39 +60,39 @@ contains
         real(real64) :: k3u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k3v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         real(real64) :: k4u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), k4v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
 
-        ! ! Step 1
-        ! call cdu(M,u,v,k1u,nu)
-        ! du1 = k1u*dt/2
-        ! call cdv(M,u,v,k1v,nu)
-        ! dv1 = k1v*dt/2
+        ! Step 1
+        call cdu(M,u,v,k1u,nu)
+        du1 = k1u*dt/2
+        call cdv(M,u,v,k1v,nu)
+        dv1 = k1v*dt/2
         
-        ! ! Step 2
-        ! call cdu(M,(u+du1),(v+dv1),k2u,nu)
-        ! du2 = k2u*dt/2
-        ! call cdv(M,(u+du1),(v+dv1),k2v,nu)
-        ! dv2 = k2v*dt/2
+        ! Step 2
+        call cdu(M,(u+du1),(v+dv1),k2u,nu)
+        du2 = k2u*dt/2
+        call cdv(M,(u+du1),(v+dv1),k2v,nu)
+        dv2 = k2v*dt/2
 
-        ! ! Step 3
-        ! call cdu(M,(u+du2),(v+dv2),k3u,nu)
-        ! du3 = dt*k3u
-        ! call cdv(M,(u+du2),(v+dv2),k3v,nu)
-        ! dv3 = dt*k3v
+        ! Step 3
+        call cdu(M,(u+du2),(v+dv2),k3u,nu)
+        du3 = dt*k3u
+        call cdv(M,(u+du2),(v+dv2),k3v,nu)
+        dv3 = dt*k3v
 
-        ! ! Step 4
-        ! call cdu(M,(u+du3),(v+dv3),k4u,nu)
-        ! call cdv(M,(u+du3),(v+dv3),k4v,nu)
+        ! Step 4
+        call cdu(M,(u+du3),(v+dv3),k4u,nu)
+        call cdv(M,(u+du3),(v+dv3),k4v,nu)
 
-        ! us = u + 1.0d0/6*dt*(k1u + 2*k2u + 2*k3u + k4u)
-        ! vs = v + 1.0d0/6*dt*(k1v + 2*k2v + 2*k3v + k4v)
+        us = u + 1.0d0/6*dt*(k1u + 2*k2u + 2*k3u + k4u)
+        vs = v + 1.0d0/6*dt*(k1v + 2*k2v + 2*k3v + k4v)
 
         ! us = us/2
         ! vs = vs/2
 
-        call cdu(M,u,v,us,nu)
-        call cdv(M,u,v,vs,nu) 
+        ! call cdu(M,u,v,us,nu)
+        ! call cdv(M,u,v,vs,nu) 
 
-        us = u + dt*us
-        vs = v + dt*vs
+        ! us = u + dt*us
+        ! vs = v + dt*vs
     end subroutine RK4
 
 end module mod_time_stepping
