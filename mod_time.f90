e71a5ba9a9c563baaf27314ee3d4774684a7ee42
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-06 23:26:54 +0530

    Added the force term in the calculation of convection and diffusion terms

--- mod_time.f90
+++ mod_time.f90
@@ -91,39 +91,42 @@ contains
         end do
     end subroutine calculate_rhs
 
-    pure subroutine cdu(M,u,v,us,nu)
+    pure subroutine cdu(M,u,v,us,nu,Fx)
         ! Calculates convection and diffusion terms for u
         class(mesh), intent(in) :: M
-        real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub) 
+        real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: Fx(M%xp%lb:M%xp%ub,M%yp%lb:M%yp%ub)
         real(real32), intent(in) :: nu 
 
         real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub)
         integer(int32) :: i,j
-        real(real64) :: vcenter, dxi, dyi
+        real(real64) :: vcenter, dxi, dyi, Fxavg
 
         dxi = 1/M%dx
         dyi = 1/M%dy
 
         ! (u-velocity cell)
         do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb+1:M%xu%ub-1)
+            Fxavg = 0.5d0*(Fx(i-1,j) + Fx(i+1,j))
             vcenter = 0.25*(v(i-1,j) + v(i-1,j+1) + v(i,j) + v(i,j+1))
             us(i,j) =   nu*(u(i-1,j) - 2*u(i,j) + u(i+1,j))*dxi**2 &
                         + nu*(u(i,j-1) -2*u(i,j) + u(i,j+1))*dyi**2 &
                         - u(i,j)*(u(i+1,j) - u(i-1,j))*0.5*dxi &
-                        - vcenter*(u(i,j+1)-u(i,j-1))*0.5*dyi
+                        - vcenter*(u(i,j+1)-u(i,j-1))*0.5*dyi + Fxavg
         end do
 
     end subroutine cdu
 
-    pure subroutine cdv(M,u,v,vs,nu)
+    pure subroutine cdv(M,u,v,vs,nu,Fy)
         ! Calculates convection and diffusion terms for u
         class(mesh), intent(in) :: M
         real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub) 
+        real(real64), intent(in) :: Fy(M%xp%lb:M%xp%ub,M%yp%lb:M%yp%ub)
         real(real32), intent(in) :: nu 
 
         real(real64), intent(in out) :: vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
         integer(int32) :: i,j
-        real(real64) :: ucenter, dxi, dyi
+        real(real64) :: ucenter, dxi, dyi, Fyavg
 
         dxi = 1/M%dx
         dyi = 1/M%dy
@@ -131,11 +134,12 @@ contains
         ! vs 
         ! (v-velocity cell)
         do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
+            Fyavg = 0.5d0*(Fx(i,j-1) + Fx(i,j+1))
             ucenter = 0.25*(u(i,j-1)+u(i,j)+u(i+1,j-1)+u(i+1,j))
             vs(i,j) =   nu*(v(i-1,j) - 2*v(i,j) + v(i+1,j))*dxi**2 &
                         + nu*(v(i,j-1) - 2*v(i,j) + v(i,j+1))*dyi**2 &
                         - ucenter*(v(i+1,j) - v(i-1,j))*0.5*dxi &
-                        - v(i,j)*(v(i,j+1)-v(i,j-1))*0.5*dyi
+                        - v(i,j)*(v(i,j+1)-v(i,j-1))*0.5*dyi + Fyavg
         end do
 
     end subroutine cdv
