5ae514b6f5fea6ca77fa49c81f5a0a11ceb0f1aa
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-07 18:06:43 +0530

    Added subroutines to write mesh to files

--- mod_io.f90
+++ mod_io.f90
@@ -1,10 +1,10 @@
 module mod_io
     use iso_fortran_env, only: int32, real32, real64
-    use mod_vec
+    use mod_mesh
     implicit none
     
     private
-    public :: write_field
+    public :: write_field, write_mesh
 contains
 
     subroutine write_field(F,fieldname,timestep)
@@ -27,21 +27,62 @@ contains
         close(fileunit)
     end subroutine write_field
 
-    subroutine write_mesh(phi_mesh,c)
-        class(vec), intent(in) :: phi_mesh(:,:)
+    subroutine write_mesh(M,c)
+        class(mesh), intent(in) :: M
         character(len=1), intent(in) :: c
 
         integer(int32) :: fileunit = 8
         character(len=:), allocatable :: filename
         integer(int32) :: i, j
 
-        filename = c // '_' // 'x' // 'mesh' //'.txt'
-        open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
-        do j = 1,size(phi_mesh,2)
-            write(fileunit, '(*(F14.7))')( real(phi_mesh(i,j)%x) , i = 1,size(phi_mesh,1))
-        end do
-        close(fileunit)
+        if (c.eq.'u') then 
+            filename = c // '_x_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yu%lb,M%yu%ub 
+                write(fileunit, '(*(F14.7))')(M%u_mesh(i,j)%x , i = M%xu%lb,M%xu%ub)
+            end do
+            close(fileunit)
 
-    end subroutine
+            filename = c // '_y_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yu%lb,M%yu%ub
+                write(fileunit, '(*(F14.7))')(M%u_mesh(i,j)%y , i = M%xu%lb,M%xu%ub)
+            end do
+            close(fileunit)
+        end if
+
+        if (c.eq.'v') then 
+            filename = c // '_x_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yv%lb,M%yv%ub 
+                write(fileunit, '(*(F14.7))')(M%v_mesh(i,j)%x , i = M%xv%lb,M%xv%ub)
+            end do
+            close(fileunit)
+
+            filename = c // '_y_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yv%lb,M%yv%ub
+                write(fileunit, '(*(F14.7))')(M%v_mesh(i,j)%y , i = M%xv%lb,M%xv%ub)
+            end do
+            close(fileunit)
+        end if
+
+        if (c.eq.'p') then
+            filename = c // '_x_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yp%lb,M%yp%ub 
+                write(fileunit, '(*(F14.7))')(M%p_mesh(i,j)%x , i = M%xp%lb,M%xp%ub)
+            end do
+            close(fileunit)
+
+            filename = c // '_y_' // 'mesh' //'.txt'
+            open(unit=fileunit, file=filename, ACTION="write", STATUS="replace")
+            do j = M%yp%lb,M%yp%ub
+                write(fileunit, '(*(F14.7))')(M%p_mesh(i,j)%y , i = M%xp%lb,M%xp%ub)
+            end do
+            close(fileunit)
+        end if
+
+    end subroutine write_mesh
 
 end module mod_io
