1b2bb089c98e674cb6ca8239c6b3be9d1848c2c3
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-07 15:31:28 +0530

    Added a vector type

---
+++ mod_vec.f90
@@ -0,0 +1,23 @@
+module mod_vec
+    use iso_fortran_env, only: int32, real32, real64
+    implicit none
+   
+    private
+    public :: vec
+
+    type :: vec
+        real(real64) :: x
+        real(real64) :: y 
+    end type 
+
+    interface vec
+        module procedure :: vec_constructor
+    end interface vec
+
+contains
+
+    pure type(vec) function vec_constructor() result(self)
+        self%x = 0.0d0
+        self%y = 0.0d0
+    end function
+end module mod_vec
