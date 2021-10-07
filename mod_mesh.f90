359484e757322823b609fec53f33afa9ee4caff5
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-07 15:31:45 +0530

    Added instructions for generating mesh

--- mod_mesh.f90
+++ mod_mesh.f90
@@ -1,5 +1,6 @@
 module mod_mesh
     use iso_fortran_env, only: int32, real32, int64, real64
+    use mod_vec
     use mod_dims
     
     implicit none
@@ -12,7 +13,7 @@ module mod_mesh
         real(real32) :: Lx, Ly, dx, dy
         integer(int32) :: Nx, Ny
         type(dims) :: xu, yu, xv, yv, xp, yp
-        real(real64), allocatable :: u_mesh(:,:), v_mesh(:,:), p_mesh(:,:)
+        type(vec), allocatable :: u_mesh(:,:), v_mesh(:,:), p_mesh(:,:)
     end type mesh
 
     interface mesh
@@ -53,27 +54,39 @@ contains
             allocate(self%u_mesh(self%xu%lb:self%xu%ub,self%yu%lb:self%yu%ub))
             allocate(self%v_mesh(self%xv%lb:self%xv%ub,self%yv%lb:self%yv%ub))
             allocate(self%p_mesh(self%xp%lb:self%xp%ub,self%yp%lb:self%yp%ub))
-           
-            ! Initialize the arrays
-            self%u_mesh = 0.0d0
-            self%v_mesh = 0.0d0
-            self%p_mesh = 0.0d0
 
         end function mesh_constructor
     
-    ! subroutine generate_mesh(self)
-    !     class(mesh), intent(in out) :: self
+    subroutine generate_mesh(self)
+        class(mesh), intent(in out) :: self
 
-    !     integer(int32) :: i, j
+        integer(int32) :: i, j
 
-    !     ! Assign coordinates to mesh points 
-    !     ! u_mesh
-    !     do concurrent (j = 1:self%Nx, i = 1:self%Ny)
-    !         u_mesh(i,j) = u_mesh(i,j)
-    !     end do
-        
-        
-    ! end subroutine generate_mesh
+        ! Assign coordinates to mesh points 
+        ! u_mesh
+        ! x-ccordinates
+        do i = 2,size(self%u_mesh,1)
+            self%u_mesh(i,:)%x = self%u_mesh(i,:)%x + self%dx 
+        end do
+        ! y-coordinates
+        ! The u cells start from below the boundary in the y-direction
+        self%u_mesh(:,1)%y = -self%dy/2.0d0
+        do j = 2,size(self%u_mesh,2)
+            self%u_mesh(:,j)%y = self%u_mesh(:,j)%y + self%dy
+        end do
+
+        ! v_mesh
+        ! x-coordinates
+        self%v_mesh(1,:)%x = -self%dx/2.0d0
+        do i = 2,size(self%v_mesh,1)
+            self%v_mesh(i,:)%x = self%v_mesh(i,:)%x + self%dx
+        end do
+        ! y-coordinates
+        ! The v cells start from the left side of the boundary in the x-direction
+        do j = 2,size(self%v_mesh),2
+            self%v_mesh(:,j)%y = self%v_mesh(:,j)%y + self%dy
+        end do
+    end subroutine generate_mesh
 
 
 end module mod_mesh
