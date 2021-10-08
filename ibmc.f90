c0444dbfe45b02d4e68d5c2e6118104edad1d9aa
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-08 12:13:14 +0530

    Changed the input arguments of the generate_laplacian subroutine

--- ibmc.f90
+++ ibmc.f90
@@ -1,7 +1,7 @@
 program ibmc
-    use iso_fortran_env, only: int32, real32, real64
-    use mod_pressure, only: generate_laplacian_sparse, calculate_pressure_sparse
-    use mod_amgx, only: calculate_pressure_amgx
+    use iso_fortran_env,    only: int32, real32, real64
+    use mod_pressure,       only: generate_laplacian_sparse, calculate_pressure_sparse
+    use mod_amgx,           only: calculate_pressure_amgx
     use mod_mesh
     use mod_time
     use mod_boundary
@@ -12,48 +12,41 @@ program ibmc
     implicit none
 
     ! Execution time
-    real(real64) :: start, finish
-
+    real(real64)    :: start, finish
     ! Computational Domain
     real(real32)    :: Lx = 1.0
     real(real32)    :: Ly = 1.0
     integer(int32)  :: Nx = 30
     integer(int32)  :: Ny = 30
-
     ! Mesh Paramaters
-    real(real32) :: dx, dxi
-    real(real32) :: dy, dyi
-
+    real(real32)    :: dx, dxi
+    real(real32)    :: dy, dyi
     ! Simulation Paramaters
-    real(real32) :: tsim    = 4
-    real(real32) :: dt      = 0.001
-    real(real32) :: t
-
+    real(real32)    :: tsim    = 4
+    real(real32)    :: dt      = 0.001
+    real(real32)    :: t
     ! Physical Constants
-    real(real32) :: nu = 1.0/100.0
-    real(real32) :: rho = 1.0d0
-
+    real(real32)    :: nu = 1.0/100.0
+    real(real32)    :: rho = 1.0d0
     ! Indices
-    integer(int32) :: i,j, it
-
+    integer(int32)  :: i,j, it
     ! Boundary values
-    real(real32) :: utop, vtop, ubottom, vbottom, &
-                    uleft, vleft, uright, vright
-
+    real(real32)    :: utop, vtop, ubottom, vbottom, &
+                       uleft, vleft, uright, vright
     ! Matrices to store fields
-    real(real64), allocatable :: u(:,:), v(:,:), us(:,:), vs(:,:), R(:,:), P(:,:), A(:,:,:), Fx(:,:), Fy(:,:)
-
+    real(real64), allocatable :: u(:,:), v(:,:), us(:,:), vs(:,:), R(:,:), &
+                                 P(:,:), A(:,:,:), Fx(:,:), Fy(:,:)
     ! Temporary/Miscellaneous variable
-    real(real64) :: ucenter, vcenter
-    integer(int32) :: NN
-    logical :: init_status
-
+    real(real64)    :: ucenter, vcenter
+    integer(int32)  :: NN
+    logical         :: init_status
     ! Mesh
-    type(mesh) :: M
-
+    type(mesh)      :: M
     ! Define an immersed boundary with a single particle
-    type(ib) :: ptcle
+    type(ib)        :: ptcle
 
+
+    !---------------------- Begin Calculations ------------------------------------!
     ! Initialize the position of the particle in the boundary and the forces
     call cpu_time(start)
 
@@ -68,7 +61,6 @@ program ibmc
     vleft   = 0.0
     uright  = 0.0
     vright  = 0.0
-
     ! Allocate matrices ofr u,v,us,vs,rhs
     allocate(u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub))    
     allocate(v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub))
@@ -80,7 +72,6 @@ program ibmc
     allocate(Fy(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub))
     NN = 5
     allocate(A(1:Nx,1:Ny,NN))
-
     ! Initialize
     u   = 0.0d0
     v   = 0.0d0
@@ -90,21 +81,16 @@ program ibmc
     A   = 0.0d0
     Fx  = 0.0d0
     Fy  = 0.0d0
-
-    ! Initialize force at the middle of the domain
-    ! Fx(12:14,12:14) = 1.0d0
-    ! Fy(12:14,12:14) = 1.0d0
-
     ! Mesh values
     dxi = 1.0/M%dx
     dyi = 1.0/M%dy
-
+    
     ! Create the IB structure
     ptcle = ib('single_particle',1)
     call update_ib(ptcle)
 
     ! Generate Laplacian matrix
-    call generate_laplacian_sparse(A,dxi,dyi)
+    call generate_laplacian_sparse(A,M%dx,M%dy)
     ! Start time loop
     t = 0.0d0
     it = 0
@@ -119,7 +105,6 @@ program ibmc
         ! Spread force from the particle
         call spread_force(M,ptcle,Fx,Fy)
 
-        ! print *, Fx
         ! Perform predictor step
         ! call predictor(M,u,v,us,vs,nu,dt) 
         call euler(M,u,v,us,vs,nu,dt,Fx,Fy)
