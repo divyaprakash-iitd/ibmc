54df54b359c7c771674e0500135572295bf557a6
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-08 16:00:18 +0530

    Modified it plot particles along with the velocity

--- vis_vel.m
+++ vis_vel.m
@@ -1,21 +1,28 @@
 clear; clc; close all;
 
 % Description: Visualizes velocity fields
-% Description: Visualizes velocity fields
+
+% Load u mesh
+xu = load('u_x_mesh.txt');
+yu = load('u_y_mesh.txt');
 
 uFile = dir(strcat('u_0','*'));
+pFile = dir(strcat('ib_','*'));
 
 nFiles = length(uFile);
 
 colormap(jet)
-colorbar
 
 for iFile = 1:nFiles
     u = load(uFile(iFile).name);
-    contourf(u,50,'edgecolor','none')
+    p = load(pFile(iFile).name);
+    contourf(xu,yu,u,50,'edgecolor','none')
+    hold on
+    plot(p(:,1),p(:,2),'w*','Markersize',10)
     title(uFile(iFile).name)
-    drawnow
+    pause(0.1)
+    if iFile ~= nFiles
+        cla
+    end
 end
-
-
 
