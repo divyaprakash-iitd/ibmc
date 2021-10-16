20767c1090cc43352e8d27573d0d122a800535aa
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-17 00:28:04 +0530

    changed lines to visualize the cilia structure

--- vis_vel.m
+++ vis_vel.m
@@ -7,7 +7,8 @@ xu = load('u_x_mesh.txt');
 yu = load('u_y_mesh.txt');
 
 uFile = dir(strcat('u_0','*'));
-pFile = dir(strcat('ib_','*'));
+p1File = dir(strcat('1_ib_','*'));
+p2File = dir(strcat('2_ib_','*'));
 
 nFiles = length(uFile);
 
@@ -18,10 +19,12 @@ figure(1)
 hold on
 for iFile = 1:nFiles
     u = load(uFile(iFile).name);
-    p = load(pFile(iFile).name);
+    p1 = load(p1File(iFile).name);
+    p2 = load(p2File(iFile).name);
     contourf(xu,yu,u,50,'edgecolor','none')
 %   p = [p ; p(1,:)];
-    plot(p(:,1),p(:,2),'w-o','Markersize',5)
+    plot(p1(:,1),p1(:,2),'w-o','Markersize',5)
+    plot(p2(:,1),p2(:,2),'w-o','Markersize',5)
     title(uFile(iFile).name)
     pause(0.001)
 %     writeVideo(v,getframe(gca));
