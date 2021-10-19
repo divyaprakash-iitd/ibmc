cd7b6b4bb8f15a04d1fb8f46cae1caadc27edbac
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-19 10:50:05 +0530

    Multiplied the point force and corrected the value of mu corresponding to stokes flow

--- compare_sol.m
+++ compare_sol.m
@@ -49,8 +49,8 @@ colormap(jet)
 
 % Cobtour plot for analytical solution
 
-mu = 1/100;
-
+mu = 1;
+F1 = 0.1;
 r = @(x,s) norm(x-s);
 
 U11 = @(x,s) 1/4/pi/mu * (-log(r(x,s)) + ((x(1)-s(1))/r(x,s)).^2);
@@ -72,8 +72,8 @@ s = [Lx/2, Ly/2];
 for j = 1:N
     for i = 1:N
         xx = [x(i,j), y(i,j)];
-        ux(i,j) = U11(xx,s);
-        uy(i,j) = U21(xx,s);
+        ux(i,j) = F1*U11(xx,s);
+        uy(i,j) = F1*U21(xx,s);
     end
 end
 
@@ -88,7 +88,7 @@ colormap(jet)
 % Location of X-planes
 np = 5;
 X = linspace(Lx/4,3*Lx/4,np);
-ymin = Ly/2; ymax = 4; N = 100;
+ymin = Ly/2; ymax = 0.6; N = 100;
 Y = linspace(ymin,ymax,N);
 
 figure(3)
@@ -97,10 +97,11 @@ for ip = 1:np
     hold on
     for j = 1:N
         xx = [X(ip), Y(j)];
-        ux = U11(xx,s);
-        uy = U21(xx,s);
+        ux = F1*U11(xx,s);
+        uy = F1*U21(xx,s);
         u = sqrt(ux^2+uy^2);
         plot(Y(j),ux,'rx')
+        plot(Y(j),interp2(xp,yp,uc,X(ip),Y(j)),'kx')
         title(sprintf('x = %.2f',X(ip)))
     end
 end
