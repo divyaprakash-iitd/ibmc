a577f1295eb2904907f082ac5829c81b249990bd
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-23 13:24:04 +0530

    Delegated the time loop to ta subroutine and running the simulation for a cavity with Re = 100

--- ibmc.f90
+++ ibmc.f90
@@ -19,25 +19,28 @@ program ibmc
     ! Code execution time
     real(real64)    :: start, finish
     ! Computational Domain
-    real(real32)    :: Lx       = 2.0
+    real(real32)    :: Lx       = 1.0
     real(real32)    :: Ly       = 1.0
     ! Mesh Paramaters
-    integer(int32)  :: Nx       = 60
+    integer(int32)  :: Nx       = 30
     integer(int32)  :: Ny       = 30
     ! Simulation time Paramaters
     real(real32)    :: tsim     = 20
     real(real32)    :: dt       = 0.0001
     real(real32)    :: t
     ! Physical Constants
-    real(real32)    :: nu       = 1.0/10
+    real(real32)    :: nu       = 1.0/100
     real(real32)    :: rho      = 1.0d0
     real(real64)    :: ks       = 0.1d0
     real(real64)    :: kb       = 1.5d0
     real(real64)    :: theta    = 3.1416
     real(real64)    :: Rl
+    real(real32)    :: FP(2)
+    real(real64)    :: SP(3)
     ! Boundary values
     real(real32)    :: utop, vtop, ubottom, vbottom, &
                        uleft, vleft, uright, vright
+    real(real32)    :: BC(8)
     ! Matrices to store fields
     real(real64), allocatable :: u(:,:), v(:,:), us(:,:), vs(:,:), R(:,:), &
                                  P(:,:), A(:,:,:), Fx(:,:), Fy(:,:)
@@ -88,7 +91,7 @@ program ibmc
     Fx  = 0.0d0
     Fy  = 0.0d0
     ! Define boundary conditions for velocity
-    utop    = 0.0
+    utop    = 1.0
     vtop    = 0.0
     ubottom = 0.0
     vbottom = 0.0
@@ -97,21 +100,13 @@ program ibmc
     uright  = 0.0
     vright  = 0.0
 
-    ! Create the IB structure
-    ! np = 1
-    ! ptcle = ib('spring_array',np)
-    ! call initialize_ib(ptcle)
-    ! ibL = 0.25
-    ! btype = 'o'
-    ! Rl = ibL/(np-1) * 0.5 ! Resting length is related to the total length of the IB
-    ! origin = vec(0.25,0.25)
-    ! call create_structure(ptcle,origin,ibL,btype) 
+    BC = [utop,vtop,ubottom,vbottom,uleft,vleft,uright,vright]
+    FP = [nu,rho]
+    SP = [ks,Rl,Ftip]
+    
     ! Generate Laplacian matrix
     call generate_laplacian_sparse(A,M%dx,M%dy)
 
-    ! Start time loop
-    t = 0.0d0
-    it = 0
     init_status = .False. ! AmgX initialization status
     call write_mesh(M,'u')
     call write_mesh(M,'v')
@@ -128,67 +123,11 @@ program ibmc
     nc = 3 ! Number of cilia
     dc = 16*M%dx ! Distance between the cilia structures
 
-    CA = cilia_array(nc,nl,np)
+    ! CA = cilia_array(nc,nl,np)
 
-    ! cil = cilia(nl,np)
-    ! call create_cilia(cil,nl,np,ibl,wbl,origin)
-    ! To-Do: Initialize the immersed boundaries in cilia with a call to initialize_ib
+    ! call create_cilia_array(CA,ibl,wbl,dc,origin)
 
-    call create_cilia_array(CA,ibl,wbl,dc,origin)
-
-    do while (t.lt.tsim)
-        t = t+dt 
-        it = it + 1
-
-        ! Apply velocity boundary conditions
-        call apply_boundary(M,u,v,utop,ubottom,uleft,uright,vtop,vbottom,vleft,vright)
-
-        ! Calculate forces in the immersed boundary structure
-        call calculate_cilia_array_force(CA,ks,Rl)
-
-        ! Apply tip force for the first 1 second
-        ! if (t.lt.0.1) then
-        call apply_tip_force_cilia_array(CA,Ftip,t)
-        ! end if
-
-        ! Spread force from the immersed boundary
-        Fx = 0.0d0 ! Initialize the forces at every time-step
-        Fy = 0.0d0
-        call spread_force_cilia_array(M,CA,Fx,Fy)
-            
-        ! Calculate intermediate/predicted velocity
-        ! call predictor(M,u,v,us,vs,nu,dt) 
-        call euler(M,u,v,us,vs,nu,dt,Fx,Fy)
-        ! call RK2(M,u,v,us,vs,nu,dt,Fx,Fy)
-        ! call RK4(M,u,v,us,vs,nu,dt,Fx,Fy)
-        
-        ! Form the right hand side of the pressure poisson equation
-        call calculate_rhs(M,us,vs,R,rho,dt)
-
-        ! Solve for presssure
-        call calculate_pressure_sparse(A,P,R)
-        ! call calculate_pressure_amgx(A,P,R,init_status)
-
-        ! Perform the corrector steps to obtain the velocity
-        call corrector(M,u,v,us,vs,p,rho,dt)
-
-        ! Initialize the velocity at every time-step
-        call initialize_velocity_cilia_array(CA)
-        ! Interpolate the Eulerian grid velocity to the Lagrangian structure
-        call interpolate_velocity_cilia_array(M,CA,u,v)
-
-        ! Update the Immersed Boundary
-        call update_cilia_array(CA,dt)
-
-        print *, 'time = ', t
-
-        ! Write files every 10th timestep
-        if (mod(it,50).eq.0) then 
-            call write_field(u,'u',it) 
-            call write_field(v,'v',it) 
-            call write_location(CA,it)
-        end if
-    end do
+    call time_loop(FP,BC,M,u,v,us,vs,Fx,Fy,SP,CA,A,P,R,tsim,dt)
 
     call cpu_time(finish)
     print '("Time = ",f6.3," seconds.")',finish-start
