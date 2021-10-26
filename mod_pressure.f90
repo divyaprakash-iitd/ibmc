2cfabb2a473189acc64f8660a8382b19afd2337c
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-26 14:13:17 +0530

    Added a subroutine to generate laplacian matrix for a periodic boundary condition

--- mod_pressure.f90
+++ mod_pressure.f90
@@ -255,4 +255,165 @@ subroutine generate_laplacian_sparse(A,dx,dy)
 
 end subroutine generate_laplacian_sparse
 
+subroutine generate_laplacian_sparse_periodic(A,dx,dy)
+
+    real(real64),   intent(in)      :: dx, dy
+    real(real64),   intent(in out)  :: A(:,:,:)
+    
+    
+    real(real64)                    :: idx2, idy2 ! Squared inverse of dx and dy
+    ! Indices
+    integer(int32) :: i,j
+    integer(int32), parameter :: BOTTOM = 1
+    integer(int32), parameter :: LEFT   = 2
+    integer(int32), parameter :: CENTER = 3
+    integer(int32), parameter :: RIGHT  = 4
+    integer(int32), parameter :: TOP    = 5
+    integer(int32), parameter :: NN     = 5
+    integer(int32) :: imin, imax, jmin, jmax
+
+    ! Define limits
+    imin = lbound(A,1)
+    imax = ubound(A,1)
+    jmin = lbound(A,2)
+    jmax = ubound(A,2)
+
+    idx2 = (1.0d0/dx)**2
+    idy2 = (1.0d0/dy)**2
+
+    ! Initialize
+    A = 0.0d0
+        
+    ! Center Nodes
+    do concurrent (j = jmin+1:jmax-1, i = imin+1:imax-1)
+        ! d^2P/dx^2
+        A(i,j,CENTER)   = -2*idx2      ! Center
+        A(i,j,RIGHT)    = idx2         ! Right
+        A(i,j,LEFT)     = idx2         ! Left
+        
+        ! d^2P/dy^2
+        A(i,j,CENTER)   = A(i,j,CENTER) + (-2*idy2)       ! Center
+        A(i,j,TOP)      = idy2                            ! Top
+        A(i,j,BOTTOM)   = idy2                            ! Bottom
+    end do
+    
+    ! Left Boundary Nodes (Excluding corners)
+    do concurrent (j = jmin+1:jmax-1)
+        do concurrent (i = imin:imin)
+            
+            ! d^2P/dx^2
+            A(i,j,LEFT)    = idx2                                ! Left
+            A(i,j,RIGHT)   = idx2                             ! Right
+            A(i,j,CENTER)  = -idx2                            ! Center
+            
+            ! d^2P/dy^2 
+            A(i,j,CENTER)  = A(i,j,CENTER) + (-2*idy2)        ! Center
+            A(i,j,BOTTOM)  = idy2                             ! Bottom
+            A(i,j,TOP)     = idy2                             ! Top                    
+        end do
+    end do
+    
+    ! Right Boundary Nodes (Excluding corners)
+    do concurrent (j = jmin+1:jmax-1)
+        do i = imax,imax
+            
+            ! d^2P/dx^2
+            A(i,j,RIGHT)    = idx2                               ! Right
+            A(i,j,LEFT)     = idx2                            ! Left
+            A(i,j,CENTER)   = -idx2                           ! Center
+            
+            ! d^2P/dy^2
+            A(i,j,CENTER)   = A(i,j,CENTER) + (-2*idy2)       ! Center
+            A(i,j,TOP)      = idy2                            ! Top
+            A(i,j,BOTTOM)   = idy2                            ! Bottom
+        end do
+    end do
+    
+    ! Top Boundary Nodes (Excluding corners)
+    do concurrent (j = jmax:jmax)
+        do concurrent (i = imin+1:imax-1)
+            
+            ! d^2P/dy^2
+            A(i,j,TOP)      = 0                               ! Top
+            A(i,j,BOTTOM)   = idy2                            ! Bottom
+            A(i,j,CENTER)   = -idy2                           ! Center
+            
+            ! d^2P/dx^2
+            A(i,j,CENTER)   = A(i,j,CENTER) + (-2*idx2)       ! Center
+            A(i,j,RIGHT)    = idx2                            ! Right
+            A(i,j,LEFT)     = idx2                            ! Left
+        end do
+    end do
+        
+    ! Bottom Boundary Nodes (Excluding corners)
+    do concurrent (j = jmin:jmin)
+        do concurrent (i = imin+1:imax-1)
+            
+            ! d^2P/dy^2
+            A(i,j,BOTTOM)   = 0                                ! Bottom
+            A(i,j,TOP)      = idy2                             ! Top
+            A(i,j,CENTER)   = -idy2                            ! Center
+            
+            ! d^2P/dx^2
+            A(i,j,CENTER)   = A(i,j,CENTER) + (-2*idx2)        ! Center
+            A(i,j,RIGHT)    = idx2                             ! Right
+            A(i,j,LEFT)     = idx2                             ! Left                    
+        end do
+    end do
+    
+    ! Corner Node (Top-Left Boundary)
+    i = imin
+    j = jmax
+    ! d^2P/dy^2
+    A(i,j,TOP)      = 0                                        ! Top
+    A(i,j,BOTTOM)   = idy2                                     ! Bottom
+    A(i,j,CENTER)   = -idy2                                    ! Center
+    ! d^2P/dx^2
+    A(i,j,LEFT)     = idx2                                        ! Right
+    A(i,j,RIGHT)    = idx2                                     ! Left
+    A(i,j,CENTER)   = A(i,j,CENTER) + (-idx2)                  ! Center
+    
+    ! Corner Node (Top-Right Boundary)
+    i = imax
+    j = jmax
+    ! d^2P/dy^2
+    A(i,j,TOP)      = 0                                        ! Top
+    A(i,j,BOTTOM)   = idy2                                     ! Bottom
+    A(i,j,CENTER)   = -idy2                                    ! Center
+    ! d^2P/dx^2
+    A(i,j,RIGHT)    = idx2                                        ! Right
+    A(i,j,LEFT)     = idx2                                     ! Left
+    A(i,j,CENTER)   = A(i,j,CENTER) + (-idx2)                  ! Center
+    
+    ! Corner Node (Bottom-Left Boundary)
+    i = imin
+    j = jmin
+    ! d^2P/dy^2
+    A(i,j,BOTTOM)   = 0                                        ! Bottom
+    A(i,j,TOP)      = idy2                                     ! Top
+    A(i,j,CENTER)   = -idy2                                    ! Center
+    ! d^2P/dx^2
+    A(i,j,LEFT)     = idx2                                        ! Right
+    A(i,j,RIGHT)    = idx2                                     ! Left
+    A(i,j,CENTER)   = A(i,j,CENTER) + (-idx2)                  ! Center
+    
+    ! Corner Node (Bottom-Right Boundary)
+    i = imax
+    j = jmin
+    ! d^2P/dy^2
+    A(i,j,BOTTOM)   = 0                                        ! Bottom
+    A(i,j,TOP)      = idy2                                     ! Top
+    A(i,j,CENTER)   = -idy2                                    ! Center
+    ! d^2P/dx^2
+    A(i,j,RIGHT)    = idx2                                        ! Right
+    A(i,j,LEFT)     = idx2                                     ! Left
+    A(i,j,CENTER)   = A(i,j,CENTER) + (-idx2)                  ! Center
+
+    ! Set the pressure at the corner cell
+    A = -A
+    A(imin,jmin,:) = 0.0d0
+    A(imin,jmin,CENTER) = 1.0d0
+
+end subroutine generate_laplacian_sparse_periodic
+
 end module mod_pressure
