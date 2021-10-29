406ea569d111f9d4b584641d1769cd7335b78089
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-29 16:55:57 +0530

    Added a cdu_p subroutine to calculate the convetive and diffusion terms according to periodic boundary condition

--- mod_time.f90
+++ mod_time.f90
@@ -220,6 +220,46 @@ contains
 
     end subroutine cdu
 
+    pure subroutine cdu_p(M,u,v,us,nu,Fx)
+        ! Calculates convection and diffusion terms for u
+        class(mesh), intent(in) :: M
+        real(real64), intent(in) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: Fx(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub)
+        real(real64), intent(in) :: nu 
+
+        real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub)
+        integer(int32) :: i,j
+        real(real64) :: vcenter, dxi, dyi 
+
+        ! Padded u at the left and right boundary for applying periodic boundary condtions
+        real(real64) :: uPad(M%xu%lb-1:M%xu%ub+1,M%yu%lb:M%yu%ub)
+
+        ! Apply boundary conditions to u via padding
+        uPad(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub) = u
+        uPad(M%xu%lb-1,:) = u(M%xu%ub,:)
+        uPad(M%xu%ub+1,:) = u(M%xu%lb,:)
+
+        ! Apply periodic boundary conditions to v
+        v(M%xv%lb,:) = v(M%xv%ub-1,:) ! Left boundary
+        v(M%xv%ub,:) = v(M%xv%lb+1,:) ! Right boundary
+
+        dxi = 1/M%dx
+        dyi = 1/M%dy
+
+        ! Perform predictor step
+       
+        ! us 
+        ! (u-velocity cell)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb:M%xu%ub)
+            vcenter = 0.25*(v(i-1,j) + v(i-1,j+1) + v(i,j) + v(i,j+1))
+            us(i,j) =   nu*(uPad(i-1,j) - 2*uPad(i,j) + uPad(i+1,j))*dxi**2 &
+                        + nu*(uPad(i,j-1) -2*uPad(i,j) + uPad(i,j+1))*dyi**2 &
+                        - uPad(i,j)*(uPad(i+1,j) - uPad(i-1,j))*0.5*dxi &
+                        - vcenter*(uPad(i,j+1)-uPad(i,j-1))*0.5*dyi + Fx(i,j)
+        end do
+
+    end subroutine cdu_p
+
     pure subroutine cdv(M,u,v,vs,nu,Fy)
         ! Calculates convection and diffusion terms for u
         class(mesh), intent(in) :: M
