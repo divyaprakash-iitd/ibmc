b51514cf25b04611184cf63dfdfe532de1ae3f24
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-10-29 15:37:32 +0530

    Added subroutines for periodic boundary condition

--- mod_time.f90
+++ mod_time.f90
@@ -43,6 +43,57 @@ contains
 
     end subroutine predictor
     
+    pure subroutine predictor_periodic(M,u,v,us,vs,nu,dt)
+        class(mesh), intent(in) :: M
+        real(real64), intent(in out) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: nu, dt
+
+        integer(int32) :: i,j
+        real(real64) :: ucenter, vcenter, dxi, dyi
+
+        ! Padded u at the left and right boundary for applying periodic boundary condtions
+        real(real64) :: uPad(M%xu%lb-1:M%xu%ub+1,M%yu%lb:M%yu%ub)
+
+        ! Apply boundary conditions to u via padding
+        uPad(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub) = u
+        uPad(M%xu%lb-1,:) = u(M%xu%ub,:)
+        uPad(M%xu%ub+1,:) = u(M%xu%lb,:)
+
+        ! Apply periodic boundary conditions to v
+        v(M%xv%lb,:) = v(M%xv%ub-1,:) ! Left boundary
+        v(M%xv%ub,:) = v(M%xv%lb+1,:) ! Right boundary
+
+        dxi = 1/M%dx
+        dyi = 1/M%dy
+
+        ! Perform predictor step
+       
+        ! us 
+        ! (u-velocity cell)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb:M%xu%ub)
+            vcenter = 0.25*(v(i-1,j) + v(i-1,j+1) + v(i,j) + v(i,j+1))
+            us(i,j) = uPad(i,j) + dt* &
+                        ( nu*(uPad(i-1,j) - 2*uPad(i,j) + uPad(i+1,j))*dxi**2 &
+                        + nu*(uPad(i,j-1) -2*uPad(i,j) + uPad(i,j+1))*dyi**2 &
+                        - uPad(i,j)*(uPad(i+1,j) - uPad(i-1,j))*0.5*dxi &
+                        - vcenter*(uPad(i,j+1)-uPad(i,j-1))*0.5*dyi)
+        end do
+
+        ! vs 
+        ! (v-velocity cell)
+        ! No changes in v-velocity calculation for periodic boundary condition
+        do concurrent (j = M%yv%lb+1:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
+            ucenter = 0.25*(u(i,j-1)+u(i,j)+u(i+1,j-1)+u(i+1,j))
+            vs(i,j) = v(i,j) + dt* &
+                        ( nu*(v(i-1,j) - 2*v(i,j) + v(i+1,j))*dxi**2 &
+                        + nu*(v(i,j-1) - 2*v(i,j) + v(i,j+1))*dyi**2 &
+                        - ucenter*(v(i+1,j) - v(i-1,j))*0.5*dxi &
+                        - v(i,j)*(v(i,j+1)-v(i,j-1))*0.5*dyi)
+        end do
+
+    end subroutine predictor_periodic
+
     pure subroutine corrector(M,u,v,us,vs,p,rho,dt)
         class(mesh), intent(in) :: M
         real(real64), intent(in out) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
@@ -69,6 +120,59 @@ contains
             v(i,j) = vs(i,j) - dt/rho * (p(i,j) - p(i,j-1)) * dyi
         end do
     end subroutine corrector
+
+    pure subroutine corrector_periodic(M,u,v,us,vs,p,rho,dt)
+        class(mesh), intent(in) :: M
+        real(real64), intent(in out) :: u(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), v(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: P(M%xp%lb:M%xp%ub,M%yp%lb:M%yp%ub)
+        real(real64), intent(in out) :: us(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub), vs(M%xv%lb:M%xv%ub,M%yv%lb:M%yv%ub)
+        real(real64), intent(in) :: rho, dt
+
+        integer(int32) :: i,j,imin,imax,jmin,jmax
+        real(real64), allocatable :: Ppad(:,:)
+        real(real64) :: dxi, dyi
+
+        ! Padded u at the left and right boundary for applying periodic boundary condtions
+        real(real64) :: uPad(M%xu%lb-1:M%xu%ub+1,M%yu%lb:M%yu%ub)
+
+        ! Apply boundary conditions to us via padding
+        uPad(M%xu%lb:M%xu%ub,M%yu%lb:M%yu%ub) = us
+        uPad(M%xu%lb-1,:) = us(M%xu%ub,:)
+        uPad(M%xu%ub+1,:) = us(M%xu%lb,:)
+
+        ! Apply periodic boundary conditions to vs
+        vs(M%xv%lb,:) = vs(M%xv%ub-1,:) ! Left boundary
+        vs(M%xv%ub,:) = vs(M%xv%lb+1,:) ! Right boundary
+        
+        ! Define limits
+        imin = lbound(P,1)
+        imax = ubound(P,1)
+        jmin = lbound(P,2)
+        jmax = ubound(P,2)
+
+        allocate(Ppad(imin-1:imax+1,jmin-1:jmax+1))
+        Ppad = 0.0d0
+        Ppad(imin:imax,jmin:jmax) = P
+        Ppad(imin-1,:) = Ppad(imax,:)
+        Ppad(imax+1,:) = Ppad(imin,:)
+
+        dxi = 1/M%dx
+        dyi = 1/M%dy
+
+        ! Perform the corrector steps
+        ! u 
+        ! (u-velocity cell)
+        do concurrent (j = M%yu%lb+1:M%yu%ub-1, i = M%xu%lb:M%xu%ub)
+            u(i,j) = uPad(i,j) - dt/rho * (Ppad(i,j) - Ppad(i-1,j)) * dxi
+        end do
+        ! 
+        ! v
+        ! (v-velocity cell)
+        do concurrent (j = M%yv%lb:M%yv%ub-1, i = M%xv%lb+1:M%xv%ub-1)
+            v(i,j) = vs(i,j) - dt/rho * (Ppad(i,j) - Ppad(i,j-1)) * dyi
+        end do
+
+    end subroutine corrector_periodic
 
     pure subroutine calculate_rhs(M,us,vs,R,rho,dt)
         class(mesh), intent(in) :: M
