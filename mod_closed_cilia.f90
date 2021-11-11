07f0d9f00ee2dff6afa5cb7e05c8bd160f6b8e3f
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-11-11 16:01:51 +0530

    Added subroutined to create closed loop array and calculate forces due to springs

--- mod_closed_cilia.f90
+++ mod_closed_cilia.f90
@@ -4,12 +4,13 @@ module mod_closed_cilia
     use mod_ib
     use mod_vec
     use mod_cilia
+    use mod_cilia_array
     implicit none
 
     real(real64), parameter :: PI = 3.141592653589793
 
     private
-    public:: create_closed_loop
+    public:: create_closed_loop, create_closed_loop_array
     
 contains
 
@@ -40,9 +41,66 @@ contains
             end do
         end do
 
-
-
-
     end subroutine create_closed_loop
-    
+   
+    subroutine create_closed_loop_array(CA,W,r,origin)
+        class(cilia_array),   intent(in out)    :: CA
+        class(vec),     intent(in)        :: origin
+        real(real64),   intent(in)        :: W          ! Width of cilia
+        real(real64),   intent(in)        :: r          ! Radius of the loop
+
+        integer(int32) :: ic
+
+        ! Assign locations to the particles of the cilia
+        do ic = 1,CA%nc
+            call create_closed_loop(CA%array(ic),W,r,origin)
+        end do
+    end subroutine create_closed_loop_array
+
+    subroutine calculate_closed_loop_array_force(CA,ko,kd,Rl)
+        class(cilia_array), intent(in out)  :: CA      ! Cilia array
+        real(real64),       intent(in)      :: ko      ! Horizontal spring stiffness
+        real(real64),       intent(in)      :: kd      ! Diagonal spring stiffness
+        real(real64),       intent(in)      :: Rl      ! Resting length
+
+        integer(int32) :: ic
+
+        ! Calculate forces for all the cilia within the array
+        do ic = 1,CA%nc
+            call calculate_closed_loop_force(CA%array(ic),ko,kd,Rl)
+        end do
+    end subroutine calculate_closed_loop_array_force
+
+    subroutine calculate_closed_loop_force(C,ko,kd,Rl)
+        class(cilia), intent(in out)    :: C         ! Cilia structure
+        real(real64), intent(in)        :: ko        ! Horizontal spring stiffness
+        real(real64), intent(in)        :: kd        ! Diagonal spring stiffness
+        real(real64), intent(in)        :: Rl        ! Resting length
+
+        character(1)    :: t = 'c'  ! Boundary type (Closed)
+        integer(int32)  :: il, ip
+
+        ! Initialize the forces to zero on all the nodes at every time step
+        do il = 1,C%nl 
+            do ip = 1,C%np
+                C%layers(il)%boundary(ip)%Fx = 0.0d0
+                C%layers(il)%boundary(ip)%Fy = 0.0d0
+            end do
+        end do
+
+        ! Calculate forces on the layers
+        do il = 1,C%nl
+            call calculate_spring_force(C%layers(il),ko,kd,Rl,t)
+        end do
+
+        ! Calculate forces on the horizontal links
+        call calculate_horizontal_link_force(C%layers(1),C%layers(2),ko,Rl)
+
+        ! Calculate forces on the diagonal links (negative slope)
+        ! call calculate_diagonal_link_force(C%layers(1),C%layers(2),kd,Rl)
+        ! call calculate_diagonal_link_force_pos(C%layers(2),C%layers(1),kd,Rl)
+    end subroutine calculate_closed_loop_force
+
+
+
 end module mod_closed_cilia
