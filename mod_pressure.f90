9c86e177481a7e6f898569310770b42db037620a
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-11-20 19:20:37 +0530

    made changes to the laplacian to incorporate the effect of pressure outlet

--- mod_pressure.f90
+++ mod_pressure.f90
@@ -339,6 +339,7 @@ subroutine generate_laplacian_sparse(A,dx,dy)
 
     ! Set the pressure at the corner cell
     A = -A
+    A(imax-1,jmin:jmax,RIGHT) = 0.0d0;
     ! A(imin,jmin,:) = 0.0d0
     ! A(imin,jmin,CENTER) = 1.0d0
 
