dceb3f66690235ac29081f4362dc55e44d819cb0
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-11-20 19:28:05 +0530

    particle and cilia simulation using amgx

--- mod_time_stepping.f90
+++ mod_time_stepping.f90
@@ -413,7 +413,7 @@ contains
             ! call calculate_pressure_sparse(A,P,R)
             ! call calculate_pressure_amgx(A,P,R,init_status)
             call calculate_pressure_amgx(A(1:M%Nx-1,:,:),P(1:99,:),R(1:M%Nx-1,:),init_status)
-            call calculate_pressure_sparse_channel(A,P,R)
+            ! call calculate_pressure_sparse_channel(A,P,R)
 
             ! Perform the corrector step to obtain the velocity
             call corrector(M,u,v,us,vs,p,rho,dt)
