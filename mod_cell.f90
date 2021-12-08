46ed7d2c246cfebeac4c28a107d8d93af550d47a
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-12-08 13:30:26 +0530

    Added a module to generate cells and for sorting particles

---
+++ mod_cell.f90
@@ -0,0 +1,41 @@
+module mod_cell
+    use iso_fortran_env, only: int32, real64, real64
+    use mod_vec
+    use mod_particle
+    implicit none
+   
+    private
+    public :: cell
+
+    type particleptr
+        type(particle), pointer :: pptr
+        integer(int32) :: ciliaId
+    end type particleptr
+
+    type :: cell
+        type(vec)                           :: loc                  ! Location of the Bottom-Left point
+        type(vec)                           :: L                    ! Dimension of the square cell
+        type(particleptr)                   :: Nlist(50)    ! Pointers of all the particles in a cell
+        integer(int32)                      :: NN ! No. of particles in the cell 
+    end type 
+
+    interface cell
+        module procedure :: cell_constructor
+    end interface cell
+
+contains
+
+    pure type(cell) function cell_constructor() result(self)
+
+        integer(int32) :: i 
+
+        self%loc%x = 0.0d0
+        self%loc%y = 0.0d0
+        self%L%x = 0.0d0
+        self%L%y = 0.0d0
+        self%NN = 0
+        do i = 1,size(self%Nlist)
+            nullify(self%Nlist(i)%pptr)
+        end do
+    end function
+end module mod_cell
