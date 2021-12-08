f160dc59090bfa89b3de7f301d0c9651f656b38a
Divyaprakash <divyaprakash.poddar@gmail.com>
2021-12-08 20:12:01 +0530

    Added lines to plot the particles

--- nlist.m
+++ nlist.m
@@ -12,3 +12,12 @@ for i = 1:size(cell_data,1) % No. of rows/entries
 end
 
 % Plot the particles in each cell
+p_data = dlmread('cell_particles.txt');
+p_data(p_data==0) = nan;
+
+% Plot the particlesplot()
+for i = 1:2:size(p_data,1) % No. of rows in the cilia file
+    px = p_data(i,:); % Read x-coordinates
+    py = p_data(i+1,:); % Read y-coordinates
+    plot(px,py,'*','linewidth',3,'Markersize',5) 
+end
