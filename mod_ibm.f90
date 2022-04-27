2f48df194066559554cc214c0a177b038ca1ce63
Divyaprakash <amz208461@am.iitd.ac.in>
2022-04-27 12:00:32 +0530

    Deformation from circle to elliptical shape

--- mod_ibm.f90
+++ mod_ibm.f90
@@ -1225,8 +1225,10 @@ contains
         ! Rl = 0.9d0*Rl
         ! alpha = (0.10d0*cos(2*PI*t/1.0d0) + 0.85d0) * (0.1d0*cos(master%theta) + 1.0d0)
         call random_number(phirnd)
-        omga = 2*PI/0.020d0
-        alpha = 1 + 0.20d0*sin(omga*t) * sin(omga*master%theta)! + phirnd*2*PI)
+        omga = 2*PI/0.04d0
+        ! alpha = 1 + 0.20d0*sin(omga*t) * sin(omga*master%theta)! + phirnd*2*PI)
+        alpha = 1 + 0.50d0*abs(sin(omga*t) * cos(omga*master%theta))! + phirnd*2*PI)
+        ! alpha = 1 + 0.40d0*sin(omga*master%theta)! + phirnd*2*PI)
         ! Rl = Rl * (0.10d0*cos(2*PI*t) + 0.85)
         ! print *, 0.9d0*sin(2*PI*t)
         
