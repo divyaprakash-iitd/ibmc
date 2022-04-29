88fbf6cdb7fa983c6beda70a0bd2ec332a4d8a8d
Divyaprakash <amz208461@am.iitd.ac.in>
2022-04-29 23:11:05 +0530

    Removed unnecessary line

--- mod_ibm.f90
+++ mod_ibm.f90
@@ -1176,40 +1176,13 @@ contains
         class(particle), intent(in) :: master, slave
         real(real64), intent(in) :: k
         real(real64), intent(in)      :: t       ! Time instant
-        
-        integer(int32), parameter :: seed = 86456
-        real(real64), allocatable :: phikm(:,:)
 
         real(real64) :: phirnd
         type(vec) :: F
         real(real64) :: xm,ym,xsl,ysl,Rl,d 
         real(real64) :: alpha
         real(real64) :: omga
-       
-        integer(int32) :: ik, im, Nk, Ntheta
-        real(real64) :: akm, wk
 
-        
-        ! Assign values to parameters
-        Nk      = 1
-        Ntheta  = 4
-        akm     = 0.02d0
-        alpha   = 1.0d0
-        wk      = 2*PI/1.0d0
-        
-        ! Generate random phase value
-        allocate(phikm(Nk,Ntheta))
-        call random_number(phikm)
-        
-        ! Calculate the value of alpha
-        do ik = 1,Nk
-            do im = 1,Ntheta
-                alpha = alpha + akm * sin(wk*t + 2*PI*phikm(ik,im)) * cos(im*master%theta)
-                ! alpha = alpha + akm * sin(wk*t) * sin(im*master%theta)
-            end do
-        end do
-
-        ! print *, alpha
         ! Calculate the original spacing between the particles
         ! Master node location
         xm = master%xo
@@ -1219,26 +1192,11 @@ contains
         xsl = slave%xo
         ysl = slave%yo
 
-
         ! Calculate distance between master and slave nodes
         Rl = norm2([(xsl-xm),(ysl-ym)])
-        ! Rl = 0.9d0*Rl
-        ! alpha = (0.10d0*cos(2*PI*t/1.0d0) + 0.85d0) * (0.1d0*cos(master%theta) + 1.0d0)
         call random_number(phirnd)
         omga = 2*PI/1.0d0
-        ! omga = 2*PI/5.0d0
-        ! alpha = 1 + 0.20d0*sin(omga*t) * sin(omga*master%theta)! + phirnd*2*PI)
-        ! alpha = 1 + 0.50d0*abs(sin(omga*t) * cos(omga*master%theta))! + phirnd*2*PI)
-        ! alpha = 1 + 0.50d0*sin(omga*t) * abs(sin(omga*master%theta)*sin(omga*slave%theta))! + phirnd*2*PI)
-        ! !alpha = 1 + 0.40d0*sin(omga*t) * abs(sin(master%theta)*sin(slave%theta))! + phirnd*2*PI)
         alpha = 1 + 0.40d0*sin(omga*t) * sin(2*master%theta)! + phirnd*2*PI)
-        ! alpha = 1 + 0.40d0*sin(omga*master%theta)! + phirnd*2*PI)
-        ! Rl = Rl * (0.10d0*cos(2*PI*t) + 0.85)
-        ! print *, 0.9d0*sin(2*PI*t)
-        
-        ! alpha = 1 + 0.50d0*sin(omga*t) * ( abs(sin(master%theta)*sin(slave%theta)) + &
-                                            ! abs(sin(2*master%theta)*sin(2*slave%theta)) )
-        ! alpha = 0.70d0
         Rl = alpha * Rl
         ! Calculate the current spacing between particles
         ! Master node location
